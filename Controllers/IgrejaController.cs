59959b7332bb7809db6e01c0e4c4e8861701950a
DESKTOP-211ALED\user <emanoel_oliveira@outlook.com>
2024-08-27 12:44:17 -0300

    adjust igreja controller

--- Controllers/IgrejaController.cs
+++ Controllers/IgrejaController.cs
@@ -48,7 +48,7 @@ namespace SibemWebApi.Controllers
         }
 
         [HttpPut("{igrejaJson}")]
-        public async Task<ActionResult<IgrejaModel>> UpdateIgreja(String igrejaJson)
+        public async Task<ActionResult<IgrejaModel>> UpdateIgreja(string igrejaJson)
         {
             IgrejaModel? igrejaModel = JsonSerializer.Deserialize<IgrejaModel>(igrejaJson);
             
